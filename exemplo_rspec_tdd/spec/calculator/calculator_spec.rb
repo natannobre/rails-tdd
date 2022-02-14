0b627e6428b35e569a9592f7647b07dd836b6f27
natannobre <natannobre37@gmail.com>
2022-02-14 18:48:57 -0300

    refactor

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -7,27 +7,16 @@ describe Calculator, "Testing Calculator's Methods" do
     it 'with positive number' do
       result = calc.sum(5, 7)
       expect(result).to eq(12)
-
-      result = calc.sum(5, 5)
-      expect(result).to eq(10)
     end
 
     it 'with negative number' do
       result = calc.sum(-5, -7)
       expect(result).to eq(-12)
-
-      result = calc.sum(-5, -5)
-      expect(result).to eq(-10)
     end
 
     it 'with negative and positive numbers' do
       result = calc.sum(5, -7)
       expect(result).to eq(-2)
-
-      result = calc.sum(-5, 5)
-      expect(result).to eq(0)
     end
-
-    xspecify 'with numbers'
   end
 end
