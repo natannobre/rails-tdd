8ff30155e322aa2a6715c3d85689722bf2b4c6a5
natannobre <natannobre37@gmail.com>
2022-02-18 12:17:43 -0300

    Tag Filters

--- exemplo_rspec_tdd/spec/matchers/all/all_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/all/all_spec.rb
@@ -2,7 +2,7 @@
 #   it { is_expected.to all (be_odd).and be_an(Integer)}
 # end
 
-describe 'all' do
+describe 'all', 'collection': true do
   it { expect([1, 7, 9]).to all( (be_odd).and be_an(Integer) ) }
   it { expect(['ruby', 'rails']).to all( be_a(String).and include('r') ) }
 end
--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1, 2, 3]) do
+describe Array.new([1, 2, 3]), :collection do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(3, 1)
