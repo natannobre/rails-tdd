f1d44434b63203ffe44f7c53954e2e7062fcab25
natannobre <natannobre37@gmail.com>
2022-02-20 13:12:33 -0300

    fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :customers
+
+  it 'Create a customer' do
+    subject.name = 'Jackson Pires'
+    subject.email = 'jackson@pires.com'
+    subject.save
+
+    expect(subject.full_name).to eq('Sr. Jackson Pires')
+  end
+ 
+  it 'Create a customer(fixtures)' do
+    customer = customers(:jackson)
+
+    expect(customer.full_name).to eq('Sr. Jackson Pires')
+  end
 end
