773a4d06fcf051c4b52a30eae11e0b35971e13e1
natannobre <natannobre37@gmail.com>
2022-02-20 13:45:35 -0300

    Gem Faker

--- test_app/spec/factories/customer.rb
+++ test_app/spec/factories/customer.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :customer do
-    name { "Jackson Pires" }
-    email { "jackson@pires.com" }
+    # name { "Jackson Pires" }
+    # email { "jackson@pires.com" }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
   end
 end
