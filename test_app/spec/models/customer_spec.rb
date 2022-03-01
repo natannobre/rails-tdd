d116f2d7fa442689712f8af29b1dafecad5bad23
natannobre <natannobre37@gmail.com>
2022-03-01 16:38:39 -0300

    Time Helpers

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -76,4 +76,14 @@ RSpec.describe Customer, type: :model do
     expect(customer.gender).to eq('F')
     expect(customer.vip).to eq(true)
   end
+
+  it 'travel_to' do
+    travel_to Time.zone.local(2004, 11, 24, 01, 04, 44) do
+      @customer = create(:customer_vip)
+    end
+
+    p @customer.created_at
+    p Time.new(2004, 11, 24, 01, 04, 44)
+    expect(@customer.created_at).to eq(Time.zone.local(2004, 11, 24, 01, 04, 44))
+  end
 end
