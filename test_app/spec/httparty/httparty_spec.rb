5fcd9e0344d0745f1bc9f3d5521765ab01c065bf
natannobre <natannobre37@gmail.com>
2022-03-01 15:43:14 -0300

    VCR com URls não determinísticas

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -6,13 +6,20 @@ describe 'HTTParty' do
     VCR.use_cassette("jsonplaceholder/posts") do
       response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
       content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)
+      expect(content_type).to match(%r{application/json})
     end
   end
 
-  it 'content-type - vcr using rspec metadata', vcr: { cassette_name: 'jsonplaceholder/posts'} do
+  it 'VCR using rspec metadata', vcr: { cassette_name: 'jsonplaceholder/posts'} do
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
+
+  it 'VCR matching on body', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4].sample}")
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
+  end
+
 end
