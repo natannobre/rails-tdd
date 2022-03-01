706e90c0a98b7c615f9b43baba2203ca44d606f6
natannobre <natannobre37@gmail.com>
2022-03-01 15:52:12 -0300

    Modos de gravação do VCR

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -22,4 +22,10 @@ describe 'HTTParty' do
     expect(content_type).to match(/application\/json/)
   end
 
+  it 'VCR recording type', vcr: { cassette_name: 'jsonplaceholder/posts', record: :new_episodes } do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
+  end
+
 end
