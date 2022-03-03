3770412bd6c8310b23ab03d18b6a6963b448d1f9
natannobre <natannobre37@gmail.com>
2022-03-03 10:27:36 -0300

    Gem Shoulda Matchers

--- test_app/spec/models/product_spec.rb
+++ test_app/spec/models/product_spec.rb
@@ -1,31 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  it 'is valid with description, price and category' do
-    product = create(:product)
-    expect(product).to be_valid
+
+  context 'Validations' do
+    it 'is valid with description, price and category' do
+      product = create(:product)
+      expect(product).to be_valid
+    end
+
+    # it 'is invalid without description' do
+    #   product = build(:product, description: nil)
+    #   product.valid?
+    #   expect(product.errors[:description]).to include("can't be blank")
+    # end
+
+    # it 'is invalid without price' do
+    #   product = build(:product, price: nil)
+    #   product.valid?
+    #   expect(product.errors[:price]).to include("can't be blank")
+    # end
+
+    # it 'is invalid without category' do
+    #   product = build(:product, category: nil)
+    #   product.valid?
+    #   expect(product.errors[:category]).to include("can't be blank")
+    # end
+
+    it { should validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { should validate_presence_of(:category) }
   end
 
-  it 'is invalid without description' do
-    product = build(:product, description: nil)
-    product.valid?
-    expect(product.errors[:description]).to include("can't be blank")
+  context 'Associations' do
+    it { is_expected.to belong_to(:category)}
   end
 
-  it 'is invalid without price' do
-    product = build(:product, price: nil)
-    product.valid?
-    expect(product.errors[:price]).to include("can't be blank")
-  end
-
-  it 'is invalid without category' do
-    product = build(:product, category: nil)
-    product.valid?
-    expect(product.errors[:category]).to include("can't be blank")
-  end
-
-  it 'return a product with full description' do
-    product = create(:product)
-    expect(product.full_description).to eq("#{product.description} - #{product.price}")
+  context 'Instance Methods' do
+    it '#full_descriptions' do
+      product = create(:product)
+      expect(product.full_description).to eq("#{product.description} - #{product.price}")
+    end
   end
 end
