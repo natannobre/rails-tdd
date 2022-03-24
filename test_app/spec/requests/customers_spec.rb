81de87ecc0ed9698ae10e8a649e1888ddc206daf
natannobre <natannobre37@gmail.com>
2022-03-23 23:00:10 -0300

    POST - JSON

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe 'Customers', type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include_json(
         [
-          id: 1,
+          id: /\d/,
           name: (be_kind_of String),
           email: (be_kind_of String)
         ]
@@ -45,7 +45,24 @@ RSpec.describe 'Customers', type: :request do
       get '/customers/1.json'
       expect(response).to have_http_status(200)
       expect(response.body).to include_json(
-        id: /\d/
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String)
+      )
+    end
+
+    it 'create - JSON' do 
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json" }
+      customers_params = attributes_for(:customer)
+
+      post '/customers', params: { customer: customers_params }, headers: headers
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers_params[:name],
+        email: customers_params.fetch(:email)
       )
     end
   end
