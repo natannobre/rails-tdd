cca0e32b2cad9107ffa1890522b3a0d05c32860c
natannobre <natannobre37@gmail.com>
2022-04-20 19:55:37 -0300

    RSpec puro com JSON

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -83,6 +83,18 @@ RSpec.describe 'Customers', type: :request do
         email: customer.email
       )
     end
+
+    it 'destroy - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { 'ACCEPT' => 'application/json' }
+
+      customer = Customer.first
+
+      expect{ delete "/customers/#{customer.id}.json", headers: headers }.to change(Customer, :count).by(-1)
+      expect(response).to have_http_status(204)
+    end
   end
 
   # describe "GET /show" do
